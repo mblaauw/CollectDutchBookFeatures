d4158f565b5962d16ee53563d5c15e82e9a2550d
MICH <michel.blaauw@gmail.com>
2014-01-10 21:37:48 +0100

    v0.2

--- scrapeAmazon.py
+++ scrapeAmazon.py
@@ -12,19 +12,20 @@ def get_bol_bookList(url):
     soup = BeautifulSoup(html, 'lxml')
 
     timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
-    columns = ['Timestamp', 'Author', 'Title', 'Pubdat', 'Rating']
+    columns = ['Timestamp', ['BOL_Key'], 'Author', 'Title', 'Pubdat', 'Rating']
 
-    # todo: Logic works but need to iterate 12 time per page. now takes first result per page
     for i in range(1,13):
         # collect and clean data
         item = BeautifulSoup(str(soup.find_all('div', 'product_content tst_searchresults_details_' + str(i))), 'lxml')
-        title = re.sub('<[^>]+>', '', str(item.find_all('a', 'product_name')))
+        unique = str(item.find_all('a', 'product_name')).split('/')[6]
+        title = str(item.find_all('a', 'product_name')).split('/')[7][25:-1]
+        title = title[:title.find('">')]
         pubdat = str(item.find_all(itemprop='datePublished'))[16:20]
         rating = str(item.find_all(itemprop='ratingValue'))[16:19]
         author = re.sub('<[^>]+>', '', str(item.find_all(itemprop='author')))
 
         # build array and then data frame
-        arr = np.array([[timestamp], [author], [title], [pubdat], [rating]]).T
+        arr = np.array([[timestamp], [unique], [author], [title], [pubdat], [rating]]).T
         if i == 1:
             df = pd.DataFrame(arr, columns=columns)
         else:
@@ -45,13 +46,15 @@ def get_bol_bookList(url):
         for i in range(1, 13):
             # collect and clean data
             item = BeautifulSoup(str(soup.find_all('div', 'product_content tst_searchresults_details_' + str(i))), 'lxml')
-            title = re.sub('<[^>]+>', '', str(item.find_all('a', 'product_name')))
+            unique = str(item.find_all('a', 'product_name')).split('/')[6]
+            title = str(item.find_all('a', 'product_name')).split('/')[7][25:-1]
+            title = title[:title.find('">')]
             pubdat = str(item.find_all(itemprop='datePublished'))[16:20]
             rating = str(item.find_all(itemprop='ratingValue'))[16:19]
             author = re.sub('<[^>]+>', '', str(item.find_all(itemprop='author')))
 
             # build array and then data frame
-            arr = np.array([[timestamp], [author], [title], [pubdat], [rating]]).T
+            arr = np.array([[timestamp], [unique], [author], [title], [pubdat], [rating]]).T
             df2 = pd.DataFrame(arr, columns=columns)
             df = concat([df, df2], ignore_index=True)
 
@@ -59,11 +62,6 @@ def get_bol_bookList(url):
     # time.sleep(1)
     return df
 
-    '''
-    # collect price block
-    # BeautifulSoup(str(soup.find_all('strong', 'bol_pricetag big')), 'lxml')
-    # price = items.find_all(itemprop='price')
-    '''
 test = get_bol_bookList(BASE_URL)
 test.to_csv('out2.csv')
 
