264ae39fd3ed959590262bf1b5c4ef448b6a9dec
MICH <michel.blaauw@gmail.com>
2014-01-12 22:18:02 +0100

    v0.2

--- scrapeBol.py
+++ scrapeBol.py
@@ -11,28 +11,71 @@ def get_bol_book_list(url):
     html = urlopen(url).read()
     soup = BeautifulSoup(html, 'lxml')
 
-    unique = (list(soup.find_all('a', 'product_name')))
+    unique = list()
+
+    for eachRow in list(soup.find_all('a', 'product_name')):
+        unique.append(eachRow.get('href'))
 
     # collect the motherload (minus one, cause less results on final page)
     # determine maxmimum results and devide bij 12 (results per page)
     total_nr_of_items = re.sub('<[^>]+>', '', str(soup.find('span', 'tst_searchresults_nrFoundItems')))
     total_nr_of_items = int(round(int(float(total_nr_of_items.replace('.', '')))/12))
 
-    # DEBUG !!
-    total_nr_of_items = 5
-
-    for eachItem in range(1, total_nr_of_items):
+    for eachItem in range(1, 3):
         print 'Scraping link number: ' + str(eachItem)
         new_url = 'http://www.bol.com/nl/l/nederlandse-boeken/nederlandse-boeken-thrillers-fantasy-nieuw/N/255+8293+14033/No/' + str(eachItem * 12) + '/section/books/index.html'
 
         html = urlopen(new_url).read()
         soup = BeautifulSoup(html, 'lxml')
 
-        unique.append(list(soup.find_all('a', 'product_name')))
+        for eachRow in list(soup.find_all('a', 'product_name')):
+            unique.append(eachRow.get('href'))
 
     return unique
 
 
+def get_bol_book_details(book_id_list):
+
+    # clean and extract ids before parse
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
+    columns = ['Timestamp', ['BOL_Key'], 'EAN', 'GPC', 'Title', 'Summary', 'Rating', 'Attributes']
+
+    for i in range(1, len(book_id_list)):
+        get_id = book_id_list[i][len(book_id_list[i])-17:len(book_id_list[i])-1]
+        print get_id
+        new_url = 'https://api.bol.com/catalog/v4/products/' + get_id + '/?apikey=AFF492148CFC4491B29E53C183B05BF2&format=xml'
+
+        # collect detailed html
+        html = urlopen(new_url).read()
+        soup = BeautifulSoup(html, 'lxml')
+
+        # trap the rating attribute. Sometimes doesnt exist. Zero them if this happens
+        rating = soup.find_all('rating')
+        if len(rating) == 0:
+            rating = str(0)
+        else:
+            rating = str(soup.products.rating.string)
+
+        arr = np.array([[timestamp],
+                        [soup.products.id.string],
+                        [soup.products.ean.string],
+                        [soup.products.gpc.string],
+                        [soup.products.title.string],
+                        [soup.products.summary.string],
+                        rating,
+                        [soup.products.attributegroups.contents]]).T
+
+        if i == 1:
+            df = pd.DataFrame(arr, columns=columns)
+        else:
+            df2 = pd.DataFrame(arr, columns=columns)
+            df = concat([df, df2], ignore_index=True)
+
+    return df
+
+
+
 # Self test
 init = get_bol_book_list(BASE_URL)
+test = get_bol_book_details(init)
 print init
