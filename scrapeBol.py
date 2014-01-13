41417870e6191ea5110d60d08d9054796270b5a4
MICH <michel.blaauw@gmail.com>
2014-01-13 19:28:17 +0100

    v0.2

--- scrapeBol.py
+++ scrapeBol.py
@@ -6,7 +6,7 @@ from urllib2 import urlopen
 
 BASE_URL = 'http://www.bol.com/nl/l/nederlandse-boeken/nederlandse-boeken-thrillers-fantasy-nieuw/N/255+8293+5260+7373+16638+14033/index.html'
 
-
+# Get list of unique product ID's
 def get_bol_book_list(url):
     html = urlopen(url).read()
     soup = BeautifulSoup(html, 'lxml')
@@ -21,7 +21,7 @@ def get_bol_book_list(url):
     total_nr_of_items = re.sub('<[^>]+>', '', str(soup.find('span', 'tst_searchresults_nrFoundItems')))
     total_nr_of_items = int(round(int(float(total_nr_of_items.replace('.', '')))/12))
 
-    for eachItem in range(1, 3):
+    for eachItem in range(1, total_nr_of_items):
         print 'Scraping link number: ' + str(eachItem)
         new_url = 'http://www.bol.com/nl/l/nederlandse-boeken/nederlandse-boeken-thrillers-fantasy-nieuw/N/255+8293+14033/No/' + str(eachItem * 12) + '/section/books/index.html'
 
@@ -33,15 +33,17 @@ def get_bol_book_list(url):
 
     return unique
 
-
+# Get details per product ID's through API
 def get_bol_book_details(book_id_list):
 
     # clean and extract ids before parse
     timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')
     columns = ['Timestamp', ['BOL_Key'], 'EAN', 'GPC', 'Title', 'Summary', 'Rating', 'Attributes']
 
-    for i in range(1, len(book_id_list)):
-        get_id = book_id_list[i][len(book_id_list[i])-17:len(book_id_list[i])-1]
+    for i in range(1, len(init)):
+        get_id = str(init[i]).split('/')[6]
+
+        #get_id = book_id_list[i][len(book_id_list[i])-17:len(book_id_list[i])-1]
         print get_id
         new_url = 'https://api.bol.com/catalog/v4/products/' + get_id + '/?apikey=AFF492148CFC4491B29E53C183B05BF2&format=xml'
 
@@ -54,21 +56,15 @@ def get_bol_book_details(book_id_list):
         if len(rating) == 0:
             rating = str(0)
         else:
-            rating = str(soup.products.rating.string)
+            rating = soup.products.rating.string
 
-        '''
-
-        NEED TO FIX THE SHAPE OF ARRAY PASSED INTO DATAFRAME 8,8 shape but get 1,8
-
-        
-        '''
         arr = np.array([[timestamp],
                         [soup.products.id.string],
                         [soup.products.ean.string],
                         [soup.products.gpc.string],
                         [soup.products.title.string],
                         [soup.products.summary.string],
-                        rating,
+                        [rating],
                         [soup.products.attributegroups.contents]]).T
 
         if i == 1:
@@ -79,9 +75,23 @@ def get_bol_book_details(book_id_list):
 
     return df
 
-
-
 # Self test
 init = get_bol_book_list(BASE_URL)
 test = get_bol_book_details(init)
+
+test.to_excel('newlist.xls')
+
+
+
+
+import pickle
+
+with open('fulllist.pickle', 'wb') as handle:
+  pickle.dump(test, handle)
+
+test = get_bol_book_details(init)
 print init
+
+
+
+    get_id = str(eachRow).split('/')[6]
