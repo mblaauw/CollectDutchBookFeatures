bc0398c4cb49dfc925564358e9b0085d8e228b84
MICH <michel.blaauw@gmail.com>
2014-01-20 13:21:11 +0100

    Merge branch 'master' of https://github.com/mblaauw/CollectDutchBookFeatures
    Conflicts:
    	scrapeBol.py

--- scrapeCrimeZoneNL.py
+++ scrapeCrimeZoneNL.py
@@ -9,79 +9,33 @@ from urllib2 import urlopen
 # http://www.crimezone.nl/web/Titels/Verschenen.htm?pagenr=1&queryElement=592958&sorton=rating%20desc
 # http://www.crimezone.nl/web/Auteurs.htm?pagenr=1&queryElement=618064
 
-l_isbn = list()
-l_rating = list()
-
-for i in range(1,440):
-    new_url = 'http://www.crimezone.nl/web/Titels/Verschenen.htm?pagenr=' + str(i) + '&queryElement=592958&sorton=rating%20desc'
-    print new_url
-
-    html = urlopen(new_url).read()
-    soup = BeautifulSoup(html, 'lxml')
-
-    isbn = soup.find_all('div', 'query-field-div title')
-    for eachTitle in isbn:
-        for i in eachTitle.find_all('a'):
-            print i['href']
-            l_isbn.append(re.findall('\d+', i['href']))
-
-    rating = soup.find_all('div', 'query-field-div rating pct100')
-    for eachRating in rating:
-        l_rating.append(eachRating.text.strip())
-
-
-result = zip(l_isbn,
-             l_rating)
-
-
-
-
-
-
-
-
-
-
-
-
 def get_titles_rating():
-    for i in range(0,440):
 
-        i = 1
+    l_isbn = list()
+    l_rating = list()
+
+    for i in range(1, 440):
         new_url = 'http://www.crimezone.nl/web/Titels/Verschenen.htm?pagenr=' + str(i) + '&queryElement=592958&sorton=rating%20desc'
         print new_url
 
         html = urlopen(new_url).read()
         soup = BeautifulSoup(html, 'lxml')
 
+        isbn = soup.find_all('div', 'query-field-div title')
+        for eachTitle in isbn:
+            for i in eachTitle.find_all('a'):
+                print i['href']
+                l_isbn.append(re.findall('\d+', i['href']))
 
-        l_person_name = list()
-        l_first_name = list()
-        l_last_name = list()
-        l_suffix = list()
-        l_avg_rating = list()
+        rating = soup.find_all('div', 'query-field-div rating pct100')
+        for eachRating in rating:
+            l_rating.append(eachRating.text.strip())
 
-        person_name = soup.find_all('div', 'query-field-div person_name')
-        title = soup.find_all('div', 'query-field-div title')
-
-
-        for eachPerson in person_name:
-            l_person_name.append(eachPerson.text.strip())
-
-        for eachFirstName in first_name:
-            l_first_name.append(eachFirstName.text.strip())
+    result = zip(l_isbn, l_rating)
+    return result
 
 
-        result = zip(l_person_name,
-                     l_last_name,
-                     l_first_name,
-                     l_suffix,
-                     l_avg_rating)
-
-
-
-
-def get_author_ratings():
+def get_author_rating():
 
     l_person_name = list()
     l_first_name = list()
@@ -123,6 +77,8 @@ def get_author_ratings():
                  l_first_name,
                  l_suffix,
                  l_avg_rating)
-
     return result
 
+# selftest
+title_rating = get_titles_rating()
+author_rating = get_author_rating()
