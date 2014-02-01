e0c6c614a25d03add8de63d15d6b34707b83dbcc
MICH <michel.blaauw@gmail.com>
2014-02-01 18:49:07 +0100

    v0.3

--- scrapeBol_cover.py
+++ scrapeBol_cover.py
@@ -3,34 +3,31 @@ if __name__ == "__main__":
     reload(sys)
     sys.setdefaultencoding("utf-8")
 
-import re, time, datetime, numpy as np, pandas as pd
-from pandas import concat
+
+import re
 from bs4 import BeautifulSoup
 from urllib2 import urlopen
 
 BASE_URL = 'http://www.bol.com/nl/l/nederlandse-boeken/nederlandse-boeken-thrillers-fantasy-thrillers/N/261+8293+5260+7373+16638/index.html'
 
 
-
-
 # Get list of unique product ID's
-def get_bol_book_list(url, test=True):
+def get_bol_book_covers(url, test=True):
     html = urlopen(url).read()
     soup = BeautifulSoup(html, 'lxml')
 
     unique = list()
-
-    for eachRow in list(soup.find_all('a', 'product_name')):
-        unique.append(eachRow.get('href'))
 
     # collect the motherload (minus one, cause less results on final page)
     # determine maxmimum results and devide bij 12 (results per page)
     total_nr_of_items = re.sub('<[^>]+>', '', str(soup.find('span', 'tst_searchresults_nrFoundItems')))
     total_nr_of_items = int(round(int(float(total_nr_of_items.replace('.', '')))/12))
 
+    # check for test run to save on waiting time
     if test:
         total_nr_of_items = 10
 
+    # Collect all IMG tages and filter out the proper ones
     for eachItem in range(1, total_nr_of_items):
         print 'Scraping link number: ' + str(eachItem)
         new_url = 'http://www.bol.com/nl/l/nederlandse-boeken/nederlandse-boeken-thrillers-fantasy-thrillers/N/261+8293/No/' + str(eachItem * 12) + '/section/books/index.html'
@@ -39,10 +36,28 @@ def get_bol_book_list(url, test=True):
         soup = BeautifulSoup(html, 'lxml')
 
         for eachRow in list(soup.find_all('img')):
-            unique.append(eachRow.get('href'))
-    # clean the list and return from function
-    unique_result = list()
-    for i in range(1, len(unique)):
-        unique_result.append(str(unique[i]).split('/')[6])
 
-    return unique_result
+            if 'itemprop=' in str(eachRow):
+                unique.append(eachRow)
+
+    # Break up links in three columns: Title, ImgUrl and ISBN
+    title = list()
+    img = list()
+    isbn = list()
+
+    for eachLine in input_list:
+            title.append(str(eachLine).split('"')[1])
+            img.append(str(eachLine).split('"')[5])
+            isbn.append(str(test.split('/')[11]).split('"')[0][:-4])
+
+    return zip(isbn, title, img)
+
+
+
+
+
+testresult = get_bol_book_covers(BASE_URL, test=False)
+parse_result = parse_image_list( testresult )
+
+
+testresult2= [i for i in testresult if i.attribute == 'img']
