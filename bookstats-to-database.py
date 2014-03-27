381f75d7655c6a31a263be346fa284257f597cec
michel.blaauw@gmail.com <michel.blaauw@gmail.com>
2014-03-27 09:54:37 +0100

    ..

--- bookstats-to-database.py
+++ bookstats-to-database.py
@@ -1,14 +1,26 @@
-__author__ = 'mich'
+#-------------------
+import sqlite3
+conn = sqlite3.connect('/Users/mich/Calibre Library/metadata.db')
+c = conn.cursor()
 
+c.execute('SELECT * FROM stocks')
+print c.fetchone()
+
+conn.close()
+
+
+
+
+
+#-------------------
+
+__author__ = 'mich'
 import os
 import sys
 import readability as red
 
-
-
 input_folder = '/Users/mich/datascience-projects/dutch-text-analytics/data/misc-nl/'
 input_ext = '*.txt'
-
 
 def getScores():
     readability_score = list()
@@ -31,7 +43,39 @@ def getScores():
                           red.Readability(txt).SMOGIndex()])
 
     return readability_score
-
-
 print getScores()
 
+#-------------------
+
+
+
+book = '/Users/mich/Downloads/Ebooks/[NL] 2010 - Schikken of stikken - Rianne Verwoert.epub'
+
+import zipfile
+from lxml import etree
+
+def get_epub_info(fname):
+    ns = {'n':'urn:oasis:names:tc:opendocument:xmlns:container',
+          'pkg': 'http://www.idpf.org/2007/opf',
+          'dc':'http://purl.org/dc/elements/1.1/'}
+    zip = zipfile.ZipFile(fname)
+
+    # find the contents metafile
+    txt = zip.read('META-INF/container.xml')
+    tree = etree.fromstring(txt)
+    cfname = tree.xpath('n:rootfiles/n:rootfile/@full-path', namespaces=ns)[0]
+
+    # grab the metadata block from the contents metafile
+    cf = zip.read(cfname)
+    tree = etree.fromstring(cf)
+    p = tree.xpath('/pkg:package/pkg:metadata', namespaces=ns)[0]
+
+    # repackage the data
+    res = {}
+    for s in ['title', 'language', 'creator', 'date', 'identifier']:
+        res[s] = p.xpath('dc:%s/text()' % (s), namespaces=ns)[0]
+
+    return res
+
+
+get_epub_info(book)
